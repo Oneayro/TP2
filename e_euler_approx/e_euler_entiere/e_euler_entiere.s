63a3d79f8c3f78bfb3a164e78df6a19699b47b02
Oneayro <orianeclaudelle@.com>
2025-02-13 23:19:42 -0500

    à vérifier

--- e_euler_approx/e_euler_entiere/e_euler_entiere.s
+++ e_euler_approx/e_euler_entiere/e_euler_entiere.s
@@ -2,64 +2,53 @@
 .globl e_euler_entiere
 
 e_euler_entiere:
-# Epilogue 
-push %ebp
+# Prologue
+push %ebp          #Ajustement pour utiliser EBP
 mov %esp,%ebp
-push %ebx
+
+subl $4, %esp      # Faire espace sur la pile pour la variable locale
+
+push %ebx          # Storer les callee-saved sur le dessus de la pile
 push %esi
 push %edi
 
 movl 8(%ebp),%esi   # esi = iteration 10 
+
 movl $0, -4(%ebp)    # Initialise la somme à 0 dans une variable locale
 
 # DEBUT COMPLETION
 
-.data
-n: .int 10
-.text
-.globl calculSomme
-
-calculSomme:
-    movl n, %ecx
-    movl (-4(%ebb)), %ebx
-    pusl %ebx
-    adl %1, %eax // 1/!0 
-    movl %eax, %ebx
-    div %eax, %ebx
-    sub %ecx, %eax
-    je finSomme
-    pusl %eax
-    jmp calculSomme
-
-finSomme:
-    popl 
-    somme
+movl %esi, %edi       //esi = n, n =10, 10 itérattions
 
 
+calcul_somme:
+    movl $1, %ebx   
+
+fact_loop:  
+    imul %edi, %ebx      
+    decl %edi
+    jnz fact_loop       //si %edi != 0, boucle n fois
     
+    movl $1, %eax
+    divl %ebx           //1/n!
+    addl %eax, -4(%ebp)
 
-    
-
-
-
-    
-
-
-
-
-
-
+    decl %esi
+    cmpl $0, $esi
+    jnz calcul_somme
 
 
 # FIN COMPLETION
 # NE RIEN MODIFIER APRES CETTE LIGNE
 end:
 addl $0x01, -4(%ebp) # cas 1/0!
-movl -4(%ebp), %eax
 
-# Prologue
-pop %edi
+
+# Epilogue
+movl -4(%ebp), %eax   # Sauver la variable locale
+
+pop %edi              # Restaurer les callee-saved
 pop %esi
 pop %ebx
-leave
-ret
+leave                 # Désallouer les variables locales et ajuster pointeur ebp
+ret                   # Retour à la fonctione
